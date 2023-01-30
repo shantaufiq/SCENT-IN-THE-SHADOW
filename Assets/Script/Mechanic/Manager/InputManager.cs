4a441b3f33f5139896c76cd5638e8f823c986c05
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2023-01-30 23:04:35 +0700

    core: fixing

---
+++ Assets/Script/Mechanic/Manager/InputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public Player playerInput;
+    public static InputManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        playerInput = new Player();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+}
