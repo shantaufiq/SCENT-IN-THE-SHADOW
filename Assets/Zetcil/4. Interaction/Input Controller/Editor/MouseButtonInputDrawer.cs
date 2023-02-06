ffeb3ae183f6f5f2376139ab0965b59f5c4de584
Maula Syabil Haqq <shirushi@student.telkomuniversity.ac.id>
2023-02-06 16:13:30 +0700

    Visual Novel and Item Interaction

---
+++ Assets/Zetcil/4. Interaction/Input Controller/Editor/MouseButtonInputDrawer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Zetcil
+{
+	[CustomPropertyDrawer( typeof(ZetInput.MouseButtonInput ) )]
+	public class MouseButtonInputDrawer : BaseInputDrawer
+	{
+		public override string ValueToString( SerializedProperty valueProperty )
+		{
+			return valueProperty.boolValue.ToString();
+		}
+	}
+}
---
+++ Assets/Zetcil/4. Interaction/Input Controller/Scripts/ZetInput/ZetDeviceMapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TechnomediaLabs;
+
+namespace Zetcil
+{
+
+    public class ZetDeviceMapper : MonoBehaviour
+    {
+
+        public bool isEnabled;
+
+        [System.Serializable]
+        public class CDeviceButton
+        {
+            [Header("Direction Mapping")]
+            public VarFloat VarButton;
+
+            [Header("Button Value")]
+            public float ButtonValue;
+        }
+
+        [Header("Device Button Settings")]
+        public bool usingDeviceButton;
+        public List<CDeviceButton> DeviceButton;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (isEnabled)
+            {
+                if (usingDeviceButton)
+                {
+                    foreach (CDeviceButton thisButton in DeviceButton)
+                    {
+                        thisButton.ButtonValue = Input.GetAxis("Horizontal");
+                        thisButton.VarButton.CurrentValue = thisButton.ButtonValue;
+                    }
+                }
+            }
+        }
+    }
+}
