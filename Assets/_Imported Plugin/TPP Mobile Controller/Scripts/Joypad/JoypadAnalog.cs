010d52d2660d2dccda1c3e0d2eb8bdb453719259
shantaufiq <shantaufiq@student.telkomuniversity.ac.id>
2023-03-31 07:55:24 +0700

    fix: joystick ui

--- Assets/_Imported Plugin/TPP Mobile Controller/Scripts/Joypad/JoypadAnalog.cs
+++ Assets/_Imported Plugin/TPP Mobile Controller/Scripts/Joypad/JoypadAnalog.cs
@@ -2,40 +2,82 @@ using UnityEngine;
 
 class JoypadAnalog : MonoBehaviour
 {
-  public UIVirtualTouchZone virtualTouchZone;
-  public RectTransform analogControl;
-  public RectTransform parentTransform;
-  public JoypadTransform joypadAnalog;
+    public UIVirtualTouchZone virtualTouchZone;
+    public RectTransform analogControl;
+    public RectTransform parentTransform;
+    public JoypadTransform joypadAnalog;
 
-  private float joystickRadius;
+    public GameObject[] possitionFocus;
 
-  void Start()
-  {
-    joystickRadius = parentTransform.sizeDelta.y / 8;
-  }
+    private float joystickRadius;
 
-  private void Update()
-  {
-    float joystickDist = Vector2.Distance(virtualTouchZone.dragPosition, virtualTouchZone.pointerPosition);
-
-    if (virtualTouchZone.isPointerUp)
+    void Start()
     {
-      joypadAnalog.ResetGO();
-      analogControl.localPosition = joypadAnalog.thisGORect.localPosition;
-    }
-    else
-    {
-      if (virtualTouchZone.isPointerDown)
-      {
-        analogControl.localPosition = Vector3.zero;
-      }
-      else
-      {
-        analogControl.position =
-          virtualTouchZone.pointerPosition + virtualTouchZone.joystickVec *
-          joystickDist;
-      }
+        joystickRadius = parentTransform.sizeDelta.y / 8;
     }
 
-  }
+    private void Update()
+    {
+        float joystickDist = Vector2.Distance(virtualTouchZone.dragPosition, virtualTouchZone.pointerPosition);
+
+        if (virtualTouchZone.isPointerUp)
+        {
+            joypadAnalog.ResetGO();
+            analogControl.localPosition = joypadAnalog.thisGORect.localPosition;
+        }
+        else
+        {
+            if (virtualTouchZone.isPointerDown)
+            {
+                analogControl.localPosition = Vector3.zero;
+            }
+            else
+            {
+                analogControl.position =
+                  virtualTouchZone.pointerPosition + virtualTouchZone.joystickVec *
+                  joystickDist;
+
+                if ((virtualTouchZone.pointerPosition.x <= 0.5f || virtualTouchZone.pointerPosition.x >= -0.5f) && virtualTouchZone.pointerPosition.y >= 0.5f)
+                {
+                    // Debug.Log($"{Time.time} Maju kedepan. handle poss: {virtualTouchZone.pointerPosition}");
+                    possitionFocus[0].SetActive(true);
+                }
+                else
+                {
+                    possitionFocus[0].SetActive(false);
+                }
+
+                if ((virtualTouchZone.pointerPosition.x <= 0.5f || virtualTouchZone.pointerPosition.x >= -0.5f) && virtualTouchZone.pointerPosition.y <= -0.5f)
+                {
+                    // Debug.Log($"{Time.time} Mundur kebelakang. handle poss: {virtualTouchZone.pointerPosition}");
+                    possitionFocus[2].SetActive(true);
+                }
+                else
+                {
+                    possitionFocus[2].SetActive(false);
+                }
+
+                if ((virtualTouchZone.pointerPosition.y <= 0.5f || virtualTouchZone.pointerPosition.y >= -0.5f) && virtualTouchZone.pointerPosition.x >= 0.5f)
+                {
+                    // Debug.Log($"{Time.time} hadap kanan. handle poss: {virtualTouchZone.pointerPosition}");
+                    possitionFocus[1].SetActive(true);
+                }
+                else
+                {
+                    possitionFocus[1].SetActive(false);
+                }
+
+                if ((virtualTouchZone.pointerPosition.y <= 0.5f || virtualTouchZone.pointerPosition.y >= -0.5f) && virtualTouchZone.pointerPosition.x <= -0.5f)
+                {
+                    // Debug.Log($"{Time.time} hadap kiri. handle poss: {virtualTouchZone.pointerPosition}");
+                    possitionFocus[3].SetActive(true);
+                }
+                else
+                {
+                    possitionFocus[3].SetActive(false);
+                }
+            }
+        }
+
+    }
 }
